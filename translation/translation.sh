1b833f74da2585ffd2e3b17f1f41a6a037f1a0e5
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2023-09-13 21:19:11 -0700

    revert

--- translation/translation.sh
+++ translation/translation.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Prepare data
-python ./prepare_data.py $1
+python ./translation/prepare_data.py $1
 
 for column in question response_j response_k
 do
     # Encode data
-    cat ./data/$1/$column | ./marian/build/spm_encode \
+    cat ./translation/data/$1/$column | ./marian/build/spm_encode \
         --model=./model/target.spm \
-        > ./data/$1/$column.encoded
+        > ./translation/data/$1/$column.encoded
 
     # Translate
-    cat ./data/$1/$column.encoded | ./marian/build/marian-decoder \
+    cat ./translation/data/$1/$column.encoded | ./marian/build/marian-decoder \
         -c ./model/decoder.yml \
         -d 0 \
         -b 6 \
@@ -20,8 +20,8 @@ do
         --maxi-batch-sort \
         --maxi-batch 100 \
         -w 2500 \
-        > ./data/$1/$column.translated
+        > ./translation/data/$1/$column.translated
 done
 
 # postprocess data
-python ./postprocess_data.py $1 $2
+python ./translation/postprocess_data.py $1 $2
