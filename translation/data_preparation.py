30d2dc53c351dbaab68165c5daf767ab71155d63
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2023-10-04 01:31:33 -0700

    optimization on the preprocessing

--- translation/data_preparation.py
+++ translation/data_preparation.py
@@ -1,15 +1,15 @@
 import datasets
 import sys
 import os
+import re
+
+from tqdm import tqdm
 
 # get terminal parameters
 args = sys.argv
 
 if len(args) < 2:
     raise Exception('Please provide dataset name')
-
-if len(args) > 2:
-    raise Exception('Too many arguments')
 
 # load dataset
 dataset = datasets.load_dataset(f"IndonesiaAI/{args[1]}", split=f'train[{args[2]}]')
@@ -28,18 +28,34 @@ with open(f'translation/data/{args[1]}/qid', 'w', encoding='utf-8') as f:
     for qid in dataset['qid']:
         f.write(str(qid) + '\n')
 
+def clean_text(text):
+    text = str(text)
+    # Remove unwanted characters
+    pattern = re.compile(r'[^\w\s.,:;?!-]')
+    text = re.sub(pattern, '', text)
+    # Insert newlines after sentence enders
+    text = re.sub('([.?!;]) ', r'\1\n', text)
+    return text.replace('\n\n', '\n')
+
 for column in columns:
     os.mkdir(f'translation/data/{args[1]}/{column}')
     
-    # save all text in the column as a file in the data folder
+    raw_data = [clean_text(text) for text in tqdm(dataset[column], desc="Processing raw data")]
+    qid_data = [(str(dataset['qid'][i])) * len(str(text).split('\n')) for i, text in enumerate(dataset[column])]
+
     with open(f'translation/data/{args[1]}/{column}/raw', 'w', encoding='utf-8') as f:
-        with open(f'translation/data/{args[1]}/{column}/qid', 'w', encoding='utf-8') as qid:
-            # write text in the dataset column to the f and 'qid' column to qid,
-            # but split the text by new line
-            for i, text in enumerate(dataset[column]):
-                for line in str(text).split('\n'):
-                    f.write(line + '\n')
-                    qid.write(str(dataset['qid'][i]) + '\n')
+        f.writelines(raw_data)
+    with open(f'translation/data/{args[1]}/{column}/qid', 'w', encoding='utf-8') as qid:
+        qid.writelines(qid_data)
+
+    # save all text in the column as a file in the data folder
+    # with open(f'translation/data/{args[1]}/{column}/raw', 'w', encoding='utf-8') as f:
+    #     with open(f'translation/data/{args[1]}/{column}/qid', 'w', encoding='utf-8') as qid:
+    #         # write text in the dataset column to the f and 'qid' column to qid,
+    #         # but split the text by new line
+    #         for i, text in tqdm(enumerate(dataset[column])):
+    #             f.write(str(text) + '\n')
+    #             qid.write((str(dataset['qid'][i]) + '\n') * len(str(text).split('\n')))
 
     # create new file with the same name and add .encoded to the end
     with open(f'translation/data/{args[1]}/{column}/encoded', 'w', encoding='utf-8') as f:
--- translation/translation.sh
+++ translation/translation.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Prepare data
-python ./translation/data_preparation.py $1
+python ./data_preparation.py $1
 
 for column in question response_j response_k;
 do
     # Encode data
-    cat ./translation/data/$1/$column/raw | ./marian/build/spm_encode \
+    cat ./data/$1/$column/raw | ./marian/build/spm_encode \
         --model=./model/target.spm \
         > ./translation/data/$1/$column/encoded
 
     # Translate
-    cat ./translation/data/$1/$column/encoded | ./marian/build/marian-decoder \
+    cat ./data/$1/$column/encoded | ./marian/build/marian-decoder \
         -c ./model/decoder.yml \
         -d 0 \
         -b 6 \
@@ -20,8 +20,8 @@ do
         --maxi-batch-sort src \
         --maxi-batch 100 \
         -w 2500 \
-        > ./translation/data/$1/$column/translated
+        > ./data/$1/$column/translated
 done
 
 # postprocess data
-python ./translation/data_postprocess.py $1 $2
+python ./data_postprocess.py $1 $2
