cac030c20f57ca5db89893f83e34a2625bd37502
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2023-10-03 23:19:18 -0700

    synchronize the translation.sh

--- translation/translation.sh
+++ translation/translation.sh
@@ -6,12 +6,12 @@ python ./translation/data_preparation.py $1
 for column in question response_j response_k;
 do
     # Encode data
-    cat ./translation/data/$1/$column | ./marian/build/spm_encode \
+    cat ./translation/data/$1/$column/raw | ./marian/build/spm_encode \
         --model=./model/target.spm \
-        > ./translation/data/$1/$column.encoded
+        > ./translation/data/$1/$column/encoded
 
     # Translate
-    cat ./translation/data/$1/$column.encoded | ./marian/build/marian-decoder \
+    cat ./translation/data/$1/$column/encoded | ./marian/build/marian-decoder \
         -c ./model/decoder.yml \
         -d 0 \
         -b 6 \
@@ -20,7 +20,7 @@ do
         --maxi-batch-sort src \
         --maxi-batch 100 \
         -w 2500 \
-        > ./translation/data/$1/$column.translated
+        > ./translation/data/$1/$column/translated
 done
 
 # postprocess data
